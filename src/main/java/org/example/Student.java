949f6ec02692cee7b408016ebc2f44fd567eb214
tob-amalitech <tob.adoba@amalitech.com>
2025-12-06 17:54:07 +0000

    Refactor getter methods in Student class

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,29 +1,30 @@
 package org.example;
 
+// Student class - abstract
 public abstract class Student implements Gradable {
     private static int counter = 0;
     private int studentId;
     private String name;
     private int age;
-
+    
     public Student(String name, int age) {
         this.studentId = ++counter;
         this.name = name;
         this.age = age;
     }
-
-    public int getStudentId() {
-        return studentId;
+    
+    public int getStudentId() { 
+        return studentId; 
     }
-
-    public String getName() {
-        return name;
+    
+    public String getName() { 
+        return name; 
     }
-
-    public int getAge() {
-        return age;
+    
+    public int getAge() { 
+        return age; 
     }
-
+    
     public abstract String getStudentType();
     public abstract void displayStudentDetails();
 }
