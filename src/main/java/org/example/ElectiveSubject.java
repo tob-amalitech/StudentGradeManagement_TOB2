4d037a381f8ce455585ae3edc76defad4a440842
tob-amalitech <tob.adoba@amalitech.com>
2025-12-06 17:58:08 +0000

    Change ElectiveSubject class to public

--- src/main/java/org/example/ElectiveSubject.java
+++ src/main/java/org/example/ElectiveSubject.java
@@ -1,17 +1,17 @@
 package org.example;
 
 // Elective subject
-class ElectiveSubject extends Subject {
-
+public class ElectiveSubject extends Subject {
+    
     public ElectiveSubject(String subjectName, String subjectCode) {
         super(subjectName, subjectCode);
     }
-
+    
     @Override
     public String getSubjectType() {
         return "Elective";
     }
-
+    
     @Override
     public void displaySubjectInfo() {
         System.out.println(getSubjectName() + " [" + getSubjectCode() + "] - Elective Subject");
