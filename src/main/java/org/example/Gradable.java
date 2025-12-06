a673fca78c7a340dfde622e1fd9e6b860e3d96e7
tob-amalitech <tob.adoba@amalitech.com>
2025-12-06 17:53:10 +0000

    Add calculateGPA method to Gradable interface

--- src/main/java/org/example/Gradable.java
+++ src/main/java/org/example/Gradable.java
@@ -3,4 +3,5 @@ package org.example;
 public interface Gradable {
     boolean isPassing(double grade);
     String getGradeLevel(double grade);
+    double calculateGPA(double grade); // NEW: GPA calculation
 }
